128410073f6234cf74d3c5660d818360bb388698
Iuliia Sokol <avare779@gmail.com>
2022-08-20 15:01:41 +0300

    Some functions added

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // All answer options
-const option1 = document.querySelector("option1"),
-    option2 = document.querySelector("option2"),
-    option3 = document.querySelector("option3"),
-    option4 = document.querySelector("option4");
+const option1 = document.getElementById("option1"),
+  option2 = document.getElementById("option2"),
+  option3 = document.getElementById("option3"),
+  option4 = document.getElementById("option4");
 // All our options
 const optionElements = document.querySelectorAll(".option");
 const question = document.getElementById("question"), //question itself
-    numberOfQuestion = document.getElementById("number-of-question"), //question number
-    numberofAllQuestions = document.getElementById("number-of-all-questions"); // amount of all questions
+  numberOfQuestion = document.getElementById("number-of-question"), //question number
+  numberOfAllQuestions = document.getElementById("number-of-all-questions"); // amount of all questions
 
 let indexOfQuestion, //index of current question
   indexOfPage = 0; // index of page
@@ -23,16 +23,28 @@ const correctAnswer = document.getElementById("correct-answer"), //amount of cor
 
 const questions = [
   {
-    question: "Как вирахувати відсоток від числа в JS",
-    options: ["Так не можна робити",
-      "Оператор: %",
-      "Помножити на кількість відсотків та поділити на 100",  
-    "Викликати метод findPercent"],
+    question: "Как вирахувати відсоток від числа в JS?",
+    options: ["Так не можна зробити", "Оператор : %", "Помножити на кількість відсотків та поділити на 100", "Викликати метод findPercent()"],
     rightAnswer: 2,
-  }
+  },
+  {
+    question: "Скільки буде 13 + 7?",
+    options: ["20", "137", "error", "undefined"],
+    rightAnswer: 0,
+  },
+  {
+    question: "На JS не можна писати?",
+    options: ["Ігри", "Скріпти для сайтів", "Десктопні застосунки", "Немає правильної відповіді"],
+    rightAnswer: 3,
+  },
+  {
+    question: "За скільки днів було створено JS?",
+    options: ["10", "Невідомо", "250", "15"],
+    rightAnswer: 0,
+  },
 ];
 
-numberofAllQuestions.innerHTML = questions.length; // display number of all questions
+numberOfAllQuestions.innerHTML = questions.length; // display number of all questions
 
 const load = () => {
   question.innerHTML = questions[indexOfQuestion].question; // question itself
@@ -40,76 +52,78 @@ const load = () => {
   option2.innerHTML = questions[indexOfQuestion].options[1];
   option3.innerHTML = questions[indexOfQuestion].options[2];
   option4.innerHTML = questions[indexOfQuestion].options[3];
-  numberofAllQuestions.innerHTML = indexOfPage + 1; //
+  numberOfQuestion.innerHTML = indexOfPage + 1; // number of current page
   indexOfPage++;
 };
 
-let completedAnswers = [];
+let completedAnswers = []; //completed answers array
 
 const randomQuestion = () => {
   let randomNumber = Math.floor(Math.random() * questions.length);
-  let hitDublicate = false;
+  let hitDublicate = false; // checking same questions
   if (indexOfPage == questions.length) {
     quizOver();
   } else {
     if (completedAnswers.length > 0) {
-      completedAnswers.forEach(item => {
+      completedAnswers.forEach((item) => {
         if (item == randomNumber) {
           hitDublicate = true;
         }
       });
-      if (hitDublicate) {
+      if (hitDublicate == true) {
         randomQuestion();
       } else {
         indexOfQuestion = randomNumber;
         load();
       }
-    };
-    if (completedAnswers = 0) {
+    }
+    if (completedAnswers.length == 0) {
       indexOfQuestion = randomNumber;
       load();
     }
-  };
+  }
   completedAnswers.push(indexOfQuestion);
 };
 
-const checkAnswer = el => {
+const checkAnswer = (el) => {
   if (el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
-    el.target.classList.add('correct');
+    el.target.classList.add("correct");
+    updateAnswerTracker("correct");
     score++;
   } else {
-    el.target.classList.add('wrong');
+    el.target.classList.add("wrong");
+    updateAnswerTracker("wrong");
   }
   disabledOptions();
-}
+};
 const disabledOptions = () => {
-  optionElements.forEach(item => {
-    item.classList.add('disabled');
+  optionElements.forEach((item) => {
+    item.classList.add("disabled");
     if (item.dataset.id == questions[indexOfQuestion].rightAnswer) {
-      item.classList.add('correct');
+      item.classList.add("correct");
     }
-   })
- }
+  });
+};
 
 const enabledOptions = () => {
-  optionElements.forEach(item => {
-    item.classList.remove('disabled', 'correct', 'wrong');
-  })
+  optionElements.forEach((item) => {
+    item.classList.remove("disabled", "correct", "wrong");
+  });
 };
 
 const answerTracker = () => {
   questions.forEach(() => {
-    const div = document.createElement('div');
-    answerTracker.appendChild('div');
-  })
+    const div = document.createElement("div");
+    answersTracker.appendChild(div);
+  });
 };
 
-const updateAnswerTracker = status => {
-  answerTracker.children[indexOfPage - 1].classList.add(`$status`);
+const updateAnswerTracker = (status) => {
+  answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
 };
 
 const validate = () => {
-  if (optionElements[].classList.contains('disabled')) {
+  if (!optionElements[0].classList.contains("disabled")) {
     alert("Select answer");
   } else {
     randomQuestion();
@@ -117,29 +131,27 @@ const validate = () => {
   }
 };
 
-btnNext.addEventListener('click', validate);
-
+btnNext.addEventListener("click", () => {
+  validate();
+});
 
 for (option of optionElements) {
-  option.addEventListener('click', e => checkAnswer(e));
+  option.addEventListener("click", (e) => checkAnswer(e));
 }
 
-
 const quizOver = () => {
-  document.querySelector('quiz-over-modal').classList.add('active');
+  document.querySelector(".quiz-over-modal").classList.add("active");
   correctAnswer.innerHTML = score;
-  numberofAllQuestions2.innerHTML = questions.length;
+  numberOfAllQuestions2.innerHTML = questions.length;
 };
 
 const tryAgain = () => {
   window.location.reload();
 };
 
-btnTryAgain.addEventListener('click', tryAgain);
+btnTryAgain.addEventListener("click", tryAgain);
 
-
-
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   randomQuestion();
   answerTracker();
 });
